9fe6d28d5bf6dc30defbcb23763367e364d7cd4f
zma8 <zainabmoosa880@gmail.com>
2025-09-27 19:49:26 +0300

    added race controller

---
+++ controllers/race.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+
+const Race = require('../models/race.js');
+const Predictions = require('../models/predictions.js');
+
+router.get('/', async (req, res) => {
+  try {
+    const allRaces = await Race.find({}).sort({ date: 1 });
+    res.render('races/index.ejs', {
+      races: allRaces
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/:id',async(req,res)=>{
+    try{
+        const race = await Race.findById(req.params.id);
+
+        let userPrediction=null;
+        if(req.session.user){
+            userPrediction= await Predictions.findOne({
+                userId: req.session.user._id,
+                raceId:req.params.id
+            });
+        }
+        res.render ('race/show.ejs',{
+            race:race,
+            userPrediction:userPrediction
+        });
+    } catch(error){
+     console.log(error);
+    res.redirect('/');
+    }
+});
+
+router.get('/:raceId/predictions',async(req,res)=>{
+    try{
+        const race=await Race.findById(req.params.raceId);
+        const predictions= await Predictions.find({raceId:req.params.raceId}).populate('userId');
+
+        res.render('race/predictions.ejs',{
+            race:race,
+            predictions:predictions
+        });
+    } catch(error){
+     console.log(error);
+    res.redirect('/');
+    }
+});
+
+router.get('/:raceId/predictions/new', async(req,res)=>{
+    try{
+        const race=await Race.findById(req.params.raceId);
+
+        const existingPrediction=await Predictions.findOne({
+            userId:req.session.user._id,
+            raceId:req.params.raceId
+        });
+
+        if(existingPrediction){
+            return res.redirect(`/predictions/${existingPrediction._id}/edit`);
+        }
+
+        res.render('predictions/new.ejs',{
+            race:race
+        });
+
+    } catch(error){
+     console.log(error);
+    res.redirect('/');
+    }
+
+});
+
+router.post('/:raceId/predictions',async(req,res)=>{
+    try{
+        req.body.userId=req.session.user._id;
+        req.body.raceId=req.params.raceId;
+
+        await Predictions.create(req.body);
+        res.redirect('/dashboard');
+    } catch(error){
+     console.log(error);
+    res.redirect('/');
+    }
+});
